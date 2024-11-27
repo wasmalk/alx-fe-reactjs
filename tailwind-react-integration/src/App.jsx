f8d79f1f4a381cbf045013f80f9f9ffbc55a7d38
Abdulwasii <malikwasiu@yahoo.com>
2024-11-27 12:44:29 +0100

    tailwind-react-integration_ss

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,7 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile';
 
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
+}
 function App() {
   const [count, setCount] = useState(0)
 
