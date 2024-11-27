961d0201e595b11527c4b25ab8b0e871c33ac08b
Abdulwasii <malikwasiu@yahoo.com>
2024-11-27 14:54:06 +0100

    tailwind-react-integration_ss_7

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,10 +6,10 @@ function UserProfile() {
         alt="User"
         className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto"
       />
-      <h1 className="text-lg sm:text-xl text-blue-800 text-center font-semibold my-4">
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 text-center font-semibold my-4">
         John Doe
       </h1>
-      <p className="text-sm sm:text-base text-gray-600 text-center">
+      <p className="text-sm sm:text-base md:text-xl text-gray-600 text-center">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
