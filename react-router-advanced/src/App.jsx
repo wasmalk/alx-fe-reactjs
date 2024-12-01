abf78f34526e83255e37a5cd6115af3bc70984e1
Abdulwasii <malikwasiu@yahoo.com>
2024-12-01 17:58:54 +0100

    react-router-advanced_3

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./AuthContext";
 import Login from "./Login";
 import Profile from "./Profile";
-import ProtectedRoute from "./ProtectedRoute";
+import BlogPost from "./BlogPost"; // Import the BlogPost component
+import ProtectedRoute from "./ProtectedRoute"; // Ensure this component is properly implemented
 
 const App = () => {
   return (
@@ -15,13 +16,28 @@ const App = () => {
               <li><a href="/">Home</a></li>
               <li><a href="/login">Login</a></li>
               <li><a href="/profile">Profile</a></li>
+              <li><a href="/blog/1">Blog Post</a></li> {/* Example blog link */}
             </ul>
           </nav>
 
           <Routes>
+            {/* Public Routes */}
             <Route path="/" element={<h1>Home Page</h1>} />
             <Route path="/login" element={<Login />} />
-            <ProtectedRoute path="/profile" element={<Profile />} />
+            <Route path="/blog/:id" element={<BlogPost />} />
+
+            {/* Protected Routes */}
+            <Route
+              path="/profile/*"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* Catch-all Route */}
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </div>
       </Router>
