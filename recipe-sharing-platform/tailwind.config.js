7cdbca515225a831e98a6dc798f8d796ecb8f923
Abdulwasii <malikwasiu@yahoo.com>
2024-12-04 15:17:42 +0100

    recipe-sharing-platform_2

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './public/index.html', 
+    './src/**/*.{js,jsx,ts,tsx}' 
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {}, 
+  plugins: [], 
 }
-
+}
